c0d1dace6abbfc1788809a749a09bbcc692a0c8e
vasukapil2196 <rohan.deshpande@torinit.ca>
2023-06-05 09:42:26 +0530

    added the bottom left cut in card

--- src/Components/Home/Career.jsx
+++ src/Components/Home/Career.jsx
@@ -2,8 +2,8 @@ const Career = () => {
   return (
     <div className="flex justify-center items-center w-full h-screen">
     <div className="w-[80%] h-[80%] mx-auto max-w-[1127px] max-h-[709px] bg-[url('src/Components/Home/JoinUs.png')] relative">
-      <div className='max-w-[500px] max-h-[414px] flex justify-center items-center absolute left-0 bg-white bg-gradient-to-tr from-transparent via-transparent to-white border-2 border-solid border-custom-white  '>
-        <div className="box-content p-10">
+      <div className='max-w-[500px] max-h-[414px] flex justify-center items-center absolute left-0 bg-white bg-gradient-to-tr from-transparent via-transparent to-white border-2 border-solid border-custom-white border-b-0' style={{background : "linear-gradient(50.38deg, rgba(0, 0, 0, 0) 5.01%, #FFFFFF 5.01%) !important"}}>
+        <div className="box-content p-10 ">
             <h2 className=" font-roboto font-bold text-4xl leading-14 flex items-center text-custom-yellow">Careers at AU</h2>
             <p className='font-roboto font-hairline text-base leading-6 flex items-center text-black'>{`Caring for our people's well-being. Providing flexibility`}
 in how we work. Inspiring everybody to shape better
